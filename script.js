2c8b35805845f4b6387b32cdbedffaeadcef2074
Antoine COULON <a.antoinecoulon@gmail.com>
2024-09-30 10:34:17 +0200

    Add: three core variables

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ function divide(n1, n2) {
     return result;
 }
 
-let numb1 = 20;
-let numb2 = 10;
+let firstInputNumber;
+let operator;
+let secondInputNumber; 
 
 console.log(divide(numb1, numb2));
