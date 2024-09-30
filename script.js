3514d86ab285607530f77dcc06bf3c65cdc85f64
Antoine COULON <a.antoinecoulon@gmail.com>
2024-09-30 10:12:41 +0200

    Add: divide function

--- script.js
+++ script.js
@@ -13,7 +13,12 @@ function multiply(n1, n2) {
     return result;
 }
 
-let numb1 = 10;
-let numb2 = 20;
+function divide(n1, n2) {
+    let result = n1 / n2;
+    return result;
+}
 
-console.log(multiply(numb1, numb2));
+let numb1 = 20;
+let numb2 = 10;
+
+console.log(divide(numb1, numb2));
